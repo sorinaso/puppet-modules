7fc2504a451c969a11b9c2c088dd33d565b00584
Alejandro Souto <sorinaso@gmail.com>
2013-09-20 18:22:11 -0300

    bug de pip - PasteScript

--- modules/eggproxy/manifests/init.pp
+++ modules/eggproxy/manifests/init.pp
@@ -9,11 +9,18 @@ $port=8888) {
     ensure => installed,
   }
 
+   # These packages are installed before due a bug on pip install PasteScript.
+  $pip_bug_packages = ['Paste', 'PasteDeploy', 'PasteScript']
+
+  package { $pip_bug_packages:
+    provider  => 'pip',
+  }
+
   package { 'collective.eggproxy':
     ensure    => '74c262a025993e7dc2074462429f51d38517172c',
     provider  => 'pip',
     source    => 'git+git://github.com/camptocamp/collective.eggproxy.git',
-    require   => Package[$eggproxy::params::dependencies],
+    require   => [Package[$eggproxy::params::dependencies], Packages[$pip_bug_packages]],
   }
 
   file { $eggproxy::params::cache_directory:
