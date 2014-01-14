7461395a8ffa9a037eab4c3838ebc445d25d7ebb
Alejandro Souto <sorinaso@gmail.com>
2014-01-14 18:20:20 -0300

    No se usan.

--- modules/admin/manifests/ubuntu/apt_autoupdate.pp
+++
@@ -1,5 +0,0 @@
-class admin::ubuntu::apt_autoupdate {
-  # apt-get update after packages.
-  exec { "apt-update": command => "/usr/bin/apt-get update" }
-  Exec["apt-update"] -> Package <| |>
-}
--- modules/admin/manifests/ubuntu/ntp_daemon.pp
+++
@@ -1,8 +0,0 @@
-class admin::ubuntu::ntp_daemon {
-  class { ntp:
-    ensure     => running,
-    enable     => true,
-    servers    => ['ntp.ubuntu.com iburst',
-                   'pool.ntp.org iburst'],
-  }
-}
