ced148b9203783e44ce0e85c5c1ddbc21384dbd0
Alejandro Souto <sorinaso@gmail.com>
2014-05-05 03:13:16 -0300

    cambiado prefijo sorinaso por puppet

--- modules/sorinaso-cmmi/spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper_acceptance'))
-
-beaker_configuration('sorinaso-cmmi') do |c|
-  beaker_install_local_module('sorinaso-common') if beaker_is_provisioning?
-end
