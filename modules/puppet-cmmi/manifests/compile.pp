2d5be1eefe7d7d345a48660fa68f0541a2d871f6
Alejandro Souto <sorinaso@gmail.com>
2014-05-28 02:09:23 -0300

    Compilacion de source

---
+++ modules/puppet-cmmi/manifests/compile.pp
@@ -0,0 +1,76 @@
+# == Class: cmmi::compile
+#
+# Compilation for cmmi given e source directory.
+#
+# === Parameters
+#
+# [*directory*]
+# Source directory.
+#
+# [*creates*]
+# Binary that compilation creates for idempotency.
+#
+# [*configure_cmd*]
+# Configure command for compilation.
+#
+# [*make_cmd*]
+# Make command for compilation.
+#
+# [*timeout*]
+# Timeout for commands.
+#
+# [*rm_build_folder*]
+# Remove build folder after compile?
+#
+# === Variables
+#
+# === Examples
+#
+# === Authors
+#
+# Alejandro Souto <sorinaso@gmail.com>
+#
+
+define cmmi::compile (
+$directory,
+$creates,
+$configure_cmd = "${directory}/configure",
+$make_cmd = "/usr/bin/make && /usr/bin/make install",
+$timeout = 600,
+$rm_build_folder = true) {
+  # Default paramaters for Exec
+  Exec {
+    timeout => $timeout,
+  }
+
+  # Configure
+  if $configure_cmd != false {
+    exec { "cmmi-compile-${name}-config":
+      command => $configure_cmd,
+      cwd     => $directory,
+      unless  => "/usr/bin/test -f ${creates}",
+    }
+  }
+
+  # compile and install
+  exec { "cmmi-compile-${name}-make-install":
+    command => "${make_cmd}",
+    cwd     => $directory,
+    unless  => "/usr/bin/test -f ${creates}",
+    require => $configure_cmd ? {
+      false   => undef,
+      default => Exec["cmmi-compile-${name}-config"],
+    }
+  }
+
+  # Remove build folder
+  if $rm_build_folder {
+    exec { "cmmi-compile-${name}-rm-build-folder":
+      command => "${cmmi::rm_cmd} -rf ${directory}",
+      require =>  Exec["cmmi-compile-${name}-make-install"],
+      onlyif  => "/usr/bin/test -d ${directory}",
+    }
+  }
+
+  Cmmi::Compile[$name] -> Class['cmmi']
+}
---
+++ modules/puppet-cmmi/spec/acceptance/compile_spec.rb
@@ -0,0 +1,97 @@
+require 'spec_helper'
+
+include SpecHelper::Compile
+
+describe 'cmmi class:' do
+  context "when compile redis { configure_cmd => false }" do
+    it "should run without errors" do
+      shell memcached_clean_cmd
+      shell memcached_download_src_cmd
+      shell memcached_uncompress_cmd
+
+      pp = <<-EOS
+        class { 'cmmi': }
+
+        cmmi::compile { 'memcached':
+          directory       => '#{memcached_src_directory}',
+          creates         => '#{memcached_binary}',
+        }
+      EOS
+
+      apply_manifest(pp, :expect_changes => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe file(memcached_binary) do
+      it { should be_executable }
+    end
+
+    describe file(memcached_src_directory) do
+      it { should_not be_directory }
+    end
+  end
+
+  context "when compile redis { configure_cmd => false }" do
+    it "should run without errors" do
+      shell redis_clean_cmd
+      shell redis_download_src_cmd
+      shell redis_uncompress_cmd
+
+      pp = <<-EOS
+        class { 'cmmi': }
+
+        cmmi::compile { 'redis':
+          directory       => '/tmp/redis-2.6.13',
+          creates         => '/usr/local/bin/redis-server',
+          configure_cmd   => false,
+        }
+      EOS
+
+      apply_manifest(pp, :expect_changes => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    redis_binaries.each do |binary|
+      describe file(binary) do
+        it { should be_executable }
+      end
+    end
+
+    describe file(redis_src_directory) do
+      it { should_not be_directory }
+    end
+  end
+
+  context "when compile redis { configure_cmd => false, rm_build_folder => false }" do
+    it "should run without errors" do
+      shell redis_clean_cmd
+      shell redis_download_src_cmd
+      shell redis_uncompress_cmd
+
+      pp = <<-EOS
+        class { 'cmmi': }
+
+        cmmi::compile { 'redis':
+          directory       => '/tmp/redis-2.6.13',
+          creates         => '/usr/local/bin/redis-server',
+          configure_cmd   => false,
+          rm_build_folder => false,
+        }
+      EOS
+
+      apply_manifest(pp, :expect_changes => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    redis_binaries.each do |binary|
+      describe file(binary) do
+        it { should be_executable }
+      end
+    end
+
+    describe file(redis_src_directory) do
+      it { should be_directory }
+    end
+  end
+end
+
