94c4aaee3727a8e953407849a9701c906a6a1d4f
Alejandro Souto <sorinaso@gmail.com>
2014-07-04 18:09:43 -0300

    Nos aseguramos que el archivo de log tenga usuario y grupo correctos

--- modules/puppet-zabbix/manifests/server/config.pp
+++ modules/puppet-zabbix/manifests/server/config.pp
@@ -1,4 +1,10 @@
 class zabbix::server::config($log_file) {
+  file { $log_file:
+    ensure  => present,
+    owner   => $zabbix::user,
+    group   => $zabbix::group,
+  } ->
+
   zabbix::server::config_line {
     'LogFile': ensure  => $log_file;
   }
