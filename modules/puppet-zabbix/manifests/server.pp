fdb201144ab137d9e5385c65967b1a32864841cf
Alejandro Souto <sorinaso@gmail.com>
2014-07-04 18:10:19 -0300

    Corregidas algunas cosas del server

--- modules/puppet-zabbix/manifests/server.pp
+++ modules/puppet-zabbix/manifests/server.pp
@@ -7,24 +7,24 @@ $database_user,
 $database_password,
 $migrate_initial_data = true
 ) {
+  class { 'zabbix::server::mysql':
+    database_name         => $database_name,
+    database_user         => $database_user,
+    database_password     => $database_password,
+    migrate_initial_data  => $migrate_initial_data,
+  } ->
+
   class { 'zabbix::server::config':
-    log_file => $zabbix::server_log_file
-  }
+    log_file => $zabbix::server_log_file,
+  } ->
 
   class { 'zabbix::server::service':
     ensure => $service_ensure,
-    enable => $service_enable
+    enable => $service_enable,
   }
 
   case $database_provider {
-    'mysql': {
-      class { 'zabbix::server::mysql':
-        database_name         => database_name,
-        database_user         => database_user,
-        database_password     => database_password,
-        migrate_initial_data  => migrate_initial_data,
-      }
-    }
+    'mysql': {}
     default: { fail("database_provider: ${database_provider} not supported.") }
   }
 
