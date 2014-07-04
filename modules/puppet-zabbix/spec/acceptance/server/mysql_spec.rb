13fb92c1a0ff95a1eb5eb8ed0ab568504257048e
Alejandro Souto <sorinaso@gmail.com>
2014-07-04 17:59:37 -0300

    Integrado todo no solo mysql separado

--- modules/puppet-zabbix/spec/acceptance/server/mysql_spec.rb
+++ modules/puppet-zabbix/spec/acceptance/server/mysql_spec.rb
@@ -20,13 +20,12 @@ describe 'zabbix::server::mysql class:' do
       end
 
       # Clean
+      shell SpecHelper::Server::Config.clean_cmd
       shell SpecHelper::Server::Service.clean_cmd
       shell SpecHelper::Server::MYSQL.clean_cmd
 
       # Instalo
       pp = <<-EOS
-        class { 'zabbix': }
-
         class { 'mysql::server': } ->
 
         mysql::db { '#{SpecHelper::Server::MYSQL.database}':
@@ -34,7 +33,12 @@ describe 'zabbix::server::mysql class:' do
            password => '#{SpecHelper::Server::MYSQL.password}',
         } ->
 
-        class { 'zabbix::server::mysql':
+        class { 'zabbix': } ->
+
+        class { 'zabbix::server':
+          service_enable    => true,
+          service_ensure    => 'running',
+          database_provider => 'mysql',
           database_name     => '#{SpecHelper::Server::MYSQL.database}',
           database_user     => '#{SpecHelper::Server::MYSQL.username}',
           database_password => '#{SpecHelper::Server::MYSQL.password}',
@@ -64,6 +68,16 @@ describe 'zabbix::server::mysql class:' do
     end
 
     describe command(SpecHelper::Server::MYSQL.check_images_migrated_cmd) do
+      it { should return_exit_status 0 }
+    end
+
+    describe file(SpecHelper::Server::Service.script_path) do
+      it { should be_owned_by SpecHelper::Server::Service.script_user }
+      it { should be_grouped_into SpecHelper::Server::Service.script_group }
+      it { should be_executable.by_user(SpecHelper::Server::Service.script_user) }
+    end
+
+    describe command(SpecHelper::Server::Service.check_running_cmd) do
       it { should return_exit_status 0 }
     end
   end
