1350d21b3af02545cbb8442ad1ce3c1279c87838
Alejandro Souto <sorinaso@gmail.com>
2015-07-07 08:01:04 -0300

    Refactor de entorno de aceptancia

--- spec_helper_acceptance/spec_helper_acceptance.rb
+++ spec_helper_acceptance/spec_helper_acceptance.rb
@@ -1,56 +1,118 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
+require 'json'
+require 'find'
 
-def beaker_module_name_without_org(module_name)
-  module_name.split('-')[1]
-end
+#def shell(cmd); puts cmd end
 
-def beaker_local_module_path(module_name)
-  File.expand_path(File.join(File.dirname(__FILE__), '..', 'modules', "#{module_name}"))
-end
+module AcceptanceTestEnvironment
+  class PuppetModule
 
-def beaker_vm_module_path(module_name)
-  "/etc/puppet/modules/#{beaker_module_name_without_org(module_name)}"
-end
+    attr_reader :name, :user, :spec
 
-def beaker_install_local_module(module_name)
-  # Borramos los modulos viejos.
-  shell("sudo rm -rf #{beaker_vm_module_path(module_name)}")
+    def initialize(spec)
+      fail "El spec #{spec} no es un modulo" if not spec =~ %r"^\w+/\w+$"
 
-  puppet_module_install(
-      :source => beaker_local_module_path(module_name),
-      :module_name => beaker_module_name_without_org(module_name)
-  )
-end
+      @spec = spec
 
-def beaker_install_module(module_name)
-  shell("puppet module install #{module_name}")
-end
+      parts = @spec.split('/')
 
-def beaker_install_puppet
-  shell('wget "https://apt.puppetlabs.com/puppetlabs-release-$(lsb_release -sc).deb" -O /tmp/puppet.deb')
-  shell('dpkg -i /tmp/puppet.deb')
-  shell('sudo apt-get update')
-  shell('sudo apt-get -y install puppet')
-end
+      @name = parts[1]
+      @user = parts[0]
+    end
 
-def beaker_is_provisioning?
-  ENV['BEAKER_provision'] != 'no'
-end
+    def from_local_path?; @user == 'sorinaso' end
 
-def beaker_configuration(module_name, &block)
-  RSpec.configure do |c|
-    # Readable test descriptions
-    c.formatter = :documentation
+    def from_forge?; !from_local_path? end
 
-    c.before :suite do
-      if beaker_is_provisioning?
-        beaker_install_puppet
+    def local_path
+      fail "El spec #{@spec} no pertenece a un modulo local" if not from_local_path?
+
+      File.expand_path(File.join(File.dirname(__FILE__), '..', 'modules', "puppet-#{@name}"))
+    end
+
+    def vm_path; "/etc/puppet/modules/#{@name}" end
+  end
+
+  class BeakerVM
+    def install_puppet_module(puppet_module)
+      if puppet_module.from_local_path?
+        install_puppet_local_module(puppet_module)
+      else
+        install_puppet_forge_module(puppet_module)
       end
+    end
 
-      beaker_install_local_module(module_name)
+    def install_puppet
+      shell('wget "https://apt.puppetlabs.com/puppetlabs-release-$(lsb_release -sc).deb" -O /tmp/puppet.deb')
+      shell('dpkg -i /tmp/puppet.deb')
+      shell('sudo apt-get update')
+      shell('sudo apt-get -y install puppet')
+    end
 
-      yield c if block_given?
+    def is_provisioning?; ENV['BEAKER_provision'] != 'no' end
+
+    private
+
+    def install_puppet_local_module(puppet_module)
+      # Borramos los modulos viejos.
+      shell("sudo rm -rf #{puppet_module.vm_path}")
+
+      # Instalamos el modulo.
+      puppet_module_install(
+          :source => puppet_module.local_path,
+          :module_name => puppet_module.name
+      )
+    end
+
+    def install_puppet_forge_module(puppet_module)
+      shell("puppet module install #{puppet_module.spec}")
+    end
+  end
+
+  def self.beaker_vm; BeakerVM.new end
+
+  def self.is_provisioning?; beaker_vm.is_provisioning? end
+
+  def self.install_puppet_module(spec); beaker_vm.install_puppet_module(PuppetModule.new(spec)) end
+
+  def self.install_puppet; beaker_vm.install_puppet end
+
+  def self.testing_module_metadata
+    results = Find.find("metadata.json").to_a
+
+    fail "Se encontro mas de un metadata.json #{results}" if results.size > 1
+
+    fail "No se encontro un metadata.json #{results}" if results.size < 1
+
+    results[0]
+
+    JSON.parse(File.read(results[0]))
+  end
+
+  def self.start(&block)
+    RSpec.configure do |c|
+      # Readable test descriptions
+      c.formatter = :documentation
+
+      c.before :suite do
+        if true#AcceptanceTestEnvironment.is_provisioning?
+          AcceptanceTestEnvironment.install_puppet
+
+          AcceptanceTestEnvironment.testing_module_metadata["dependencies"].each do |d|
+            AcceptanceTestEnvironment.install_puppet_module(d["name"])
+          end
+        end
+
+        AcceptanceTestEnvironment.install_puppet_module(
+            AcceptanceTestEnvironment.testing_module_metadata["name"].gsub("-", "/")
+        )
+
+        yield c if block_given?
+      end
     end
   end
 end
+
+
+
